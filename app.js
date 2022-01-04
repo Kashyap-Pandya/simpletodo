2d0b455abee256e8ec750bf54691e0766c4faeb8
Kashyap Pandya <17811129+Kashyap-Pandya@users.noreply.github.com>
2022-01-04 22:53:35 +0530

    [ADD] intial

---
+++ app.js
@@ -0,0 +1,70 @@
+const addForm = document.querySelector(".add");
+const list = document.querySelector(".todos")
+const search = document.querySelector(".search input");
+
+
+const generateTemplate = todo => {
+
+    const html = `
+    <li class="list-group-item d-flex justify-content-between align-items-center">
+        <span> ${todo} </span>
+        <i class="far fa-trash-alt delete"></i>
+    </li>
+    `;
+
+    list.innerHTML += html;
+}
+
+// adding todos
+
+addForm.addEventListener("submit", e => {
+
+    e.preventDefault();
+    const todo = addForm.add.value.trim();
+
+    if (todo.length) { // it will only add if input has any length
+        generateTemplate(todo);
+        addForm.reset(); // reset the todo input
+    }
+
+
+});
+
+// Delete todos 
+
+list.addEventListener("click", e => {
+
+    if (e.target.classList.contains("delete")) {
+        e.target.parentElement.remove();
+    }
+
+    // zero todos - completed all
+
+    if (list.children.length === 0) {
+        const noTodo = `
+            <p class="relax" >Yeyyy you have finished everything for now, Relax. or add more todos</p>
+        `;
+        list.innerHTML += noTodo;
+    } else {
+        list.children.classList.add("filtered");
+    }
+})
+
+
+const filterTodos = (term) => {
+    Array.from(list.children)
+        .filter(todo => !todo.textContent.toLowerCase().includes(term))
+        .forEach(todo => todo.classList.add("filtered"))
+
+    Array.from(list.children)
+        .filter(todo => todo.textContent.toLowerCase().includes(term))
+        .forEach(todo => todo.classList.remove("filtered"))
+}
+
+//key up event
+
+search.addEventListener("keyup", () => {
+
+    const term = search.value.trim().toLowerCase();
+    filterTodos(term);
+})
